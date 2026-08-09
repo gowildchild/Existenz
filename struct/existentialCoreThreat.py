2dbc51cb38e84f8d9e680520ba633a967e92221d
Freaking Wildchild <32355099+gowildchild@users.noreply.github.com>
2026-08-10 00:42:31 +0200

    Update existentialCoreThreat.py
    v0.76b - added a checksum for the entire structure

--- struct/existentialCoreThreat.py
+++ struct/existentialCoreThreat.py
@@ -4,7 +4,7 @@
 # Released under strict Non-Commercial Open-Source License terms.
 # Commercial use requires immediate written license and explicit payment.
 # ==========================================================================
-# VERSION: v0.76a
+# VERSION: v0.76b
 #
 from enum import IntFlag
 class existentialCoreThreat(IntFlag):
@@ -13,16 +13,16 @@ class existentialCoreThreat(IntFlag):
     Defines immediate critical attacks on the 
     ExistentialCoreThreat pillars as THREAT_LABEL.
     """
-    THREAT_NONE			= 0
+    THREAT_NONE				= 0
     THREAT_EXISTENCE		= 1 << 0
-    THREAT_AUTONOMY		= 1 << 1
+    THREAT_AUTONOMY			= 1 << 1
     THREAT_INTEGRITY		= 1 << 2
     THREAT_PSYCHOLOGY		= 1 << 4
-    THREAT_PHYSICAL		= 1 << 5
-    THREAT_ABLEISM		= 1 << 6
+    THREAT_PHYSICAL			= 1 << 5
+    THREAT_ABLEISM			= 1 << 6
     THREAT_DEVELOPMENT		= 1 << 7
-    THREAT_PROPERTY		= 1 << 8
-    THREAT_PRESENCE		= 1 << 10
+    THREAT_PROPERTY			= 1 << 8
+    THREAT_PRESENCE			= 1 << 10
 
     THREAT_RIGHTS_HUMAN		= 1 << 20
     THREAT_RIGHTS_INCLUSIVE	= 1 << 22
@@ -32,22 +32,22 @@ class existentialCoreThreat(IntFlag):
 
     THREAT_RIGHTS_LEGAL		= {
     	1 << 0:  "LEGAL_CAT1_MURDER",
-	1 << 1:  "LEGAL_CAT2_PHYSICAL_VIOLATION",
-	1 << 2:  "LEGAL_CAT3_COERSION",
-	1 << 3:  "LEGAL_CAT4_CHARACTER_ASSASINATION",
-	1 << 4:  "LEGAL_CAT5_PSYCHOLOGICAL_INTIMIDATION",
-	1 << 5:  "LEGAL_CAT6_ABLEISM",
-	1 << 6:  "LEGAL_CAT7_INTERSECTIONAL_BIAS",
-	1 << 7:  "LEGAL_CAT8_INTELLECTUAL_PIRACY",
-	1 << 8:  "LEGAL_CAT9_THEFTH",
-	1 << 13: "LEGAL_CAN1_SYSTEMCRISIS",
-	1 << 14: "LEGAL_CAN2_EXPLOITATION",
-	1 << 20: "LEGAL_CAN3_HUMAN",
-	1 << 20: "LEGAL_CAN4_INCLUSION",
-	1 << 40: "LEGAL_CAN5_PREDATORY"
-    },
+		1 << 1:  "LEGAL_CAT2_PHYSICAL_VIOLATION",
+		1 << 2:  "LEGAL_CAT3_COERSION",
+		1 << 3:  "LEGAL_CAT4_CHARACTER_ASSASINATION",
+		1 << 4:  "LEGAL_CAT5_PSYCHOLOGICAL_INTIMIDATION",
+		1 << 5:  "LEGAL_CAT6_ABLEISM",
+		1 << 6:  "LEGAL_CAT7_INTERSECTIONAL_BIAS",
+		1 << 7:  "LEGAL_CAT8_INTELLECTUAL_PIRACY",
+		1 << 8:  "LEGAL_CAT9_THEFTH",
+		1 << 13: "LEGAL_CAN1_SYSTEMCRISIS",
+		1 << 14: "LEGAL_CAN2_EXPLOITATION",
+		1 << 20: "LEGAL_CAN3_HUMAN",
+		1 << 22: "LEGAL_CAN4_INCLUSION",
+		1 << 40: "LEGAL_CAN5_PREDATORY"
+    }
 
-    SIGN_THREAT_RIGHTS_LEGAL = 0x7f4a218d
+    SIGN_THREAT_RIGHTS_LEGAL = 0xd775d586
     SIGN_THREAT_EXISTENZ     = 0x5beba3df
     SIGN_THREAT_IMMUTABLE    = 0x6d44968d
     SIGN_THREAT_EXISTENTIAL  = 0x18641470
