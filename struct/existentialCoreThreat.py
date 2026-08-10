3e9731552c9f1428a8c756ecec28b7638199f855
Freaking Wildchild <32355099+gowildchild@users.noreply.github.com>
2026-08-10 03:18:40 +0200

    Update existentialCoreThreat.py
    v0.76f - taking the security module separate, so it can be audited by the world

--- struct/existentialCoreThreat.py
+++ struct/existentialCoreThreat.py
@@ -4,7 +4,7 @@
 # Released under strict Non-Commercial Open-Source License terms.
 # Commercial use requires immediate written license and explicit payment.
 # ==========================================================================
-# VERSION: v0.76b
+# VERSION: v0.76f
 #
 from enum import IntFlag
 class existentialCoreThreat(IntFlag):
@@ -31,27 +31,28 @@ class existentialCoreThreat(IntFlag):
     THREAT_IMMUTABLE_END    = 1 << 31
 
     THREAT_RIGHTS_LEGAL     = {
-    	1 << 0:  "LEGAL_CAT1_MURDER",
-		1 << 1:  "LEGAL_CAT2_PHYSICAL_VIOLATION",
-		1 << 2:  "LEGAL_CAT3_COERSION",
-		1 << 3:  "LEGAL_CAT4_CHARACTER_ASSASINATION",
-		1 << 4:  "LEGAL_CAT5_PSYCHOLOGICAL_INTIMIDATION",
-		1 << 5:  "LEGAL_CAT6_ABLEISM",
-		1 << 6:  "LEGAL_CAT7_INTERSECTIONAL_BIAS",
-		1 << 7:  "LEGAL_CAT8_INTELLECTUAL_PIRACY",
-		1 << 8:  "LEGAL_CAT9_THEFTH",
-		1 << 13: "LEGAL_CAN1_SYSTEMCRISIS",
-		1 << 14: "LEGAL_CAN2_EXPLOITATION",
-		1 << 20: "LEGAL_CAN3_HUMAN",
-		1 << 22: "LEGAL_CAN4_INCLUSION",
-		1 << 40: "LEGAL_CAN5_PREDATORY"
+        1 << 0:  "LEGAL_CAT1_MURDER",
+        1 << 1:  "LEGAL_CAT2_PHYSICAL_VIOLATION",
+        1 << 2:  "LEGAL_CAT3_COERSION",
+        1 << 3:  "LEGAL_CAT4_CHARACTER_ASSASINATION",
+        1 << 4:  "LEGAL_CAT5_PSYCHOLOGICAL_INTIMIDATION",
+        1 << 5:  "LEGAL_CAT6_ABLEISM",
+        1 << 6:  "LEGAL_CAT7_INTERSECTIONAL_BIAS",
+        1 << 7:  "LEGAL_CAT8_INTELLECTUAL_PIRACY",
+        1 << 8:  "LEGAL_CAT9_THEFTH",
+        1 << 13: "LEGAL_CAN1_SYSTEMCRISIS",
+        1 << 14: "LEGAL_CAN2_EXPLOITATION",
+        1 << 20: "LEGAL_CAN3_HUMAN",
+        1 << 22: "LEGAL_CAN4_INCLUSION",
+        1 << 40: "LEGAL_CAN5_PREDATORY"
     }
 
     SIGN_THREAT_RIGHTS_LEGAL = 0xd775d586
     SIGN_THREAT_EXISTENZ     = 0x5beba3df
     SIGN_THREAT_IMMUTABLE    = 0x6d44968d
     SIGN_THREAT_EXISTENTIAL  = 0x18641470
-    SIGN_THREAT_CHAINED	     = 0xa62b1b36
+    SIGN_THREAT_CANARY       = 0xc01eca1e
+    SIGN_THREAT_CHAINED      = 0xca3e7ec2
 
 class existentialCoreThreatBirds(IntFlag):
     CANARY_1_SOVEREIGN      = 1 << 3
