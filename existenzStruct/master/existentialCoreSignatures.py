acc6ada9c5564614a2a748a6d25d8ab33120d7fe
Freaking Wildchild <32355099+gowildchild@users.noreply.github.com>
2026-08-21 00:56:16 +0200

    Update existentialCoreSignatures.py

--- existenzStruct/master/existentialCoreSignatures.py
+++ existenzStruct/master/existentialCoreSignatures.py
@@ -13,6 +13,7 @@ class existentialCoreSignatures:
     existentialCore                              = "22023c142c21687803a3cdedb82684973d7ab5bb601b2b35d0bd8b448e26f99e"
     existentialCoreThreatRoot                    = "1f95497bb174e069c2b727d8b72a7d556a03c0db451dcf2bac6b00bf191291ca"
     existentialCoreThreatLegal                   = "931547edaba6ec457f2b6a22ef1961d56c08a765983036cb95642aa75fbd0ab1"
+    existentialCoreThreatShadowVacuum            = ""    
     existentialCoreThreat                        = "90f9d7438a0497f54dbe065a64f5f5c111dee8c1184832c280d96c0bd2226689"
     existentialCoreCheck                         = "7c165b322566e304975917cdc92de03d3ab14e72d1edaefc2ea18f7444ac891c"
 
@@ -29,5 +30,6 @@ class existentialCoreSignatures:
         ("CoreThreatStruct", "existentialCoreThreatStructSign", "existentialCoreThreatStructHash", "existentialCoreThreatStructSignature", 31, 1),
         ("CoreThreatLegal", "existentialCoreThreatLegalSign", "existentialCoreThreatLegalHash", "existentialCoreThreatLegalSignature", 31, 2),
         ("CoreThreat", "existentialCoreThreatSign", "existentialCoreThreatHash", "existentialCoreThreatSignature", 127, 3),
+        ("CoreThreatShadowVacuum", "existentialCoreThreatShadowVacuumSign", "existentialCoreThreatShadowVacuumHash", "existentialCoreThreatShadowVacuumSignature", 31, 5),        
         ("CoreChain", "existentialCoreChainSign", "existentialCoreChainHash", "existentialCoreChainSignature", 255, 9)
     )
