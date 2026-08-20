455867384e622076fb0f3b0e9a7372b9d678ffe8
Freaking Wildchild <32355099+gowildchild@users.noreply.github.com>
2026-08-20 22:20:14 +0200

    Update existentialCoreSignatures.py

--- existenzStruct/master/existentialCoreSignatures.py
+++ existenzStruct/master/existentialCoreSignatures.py
@@ -1,19 +1,19 @@
 # ==========================================================================
-# EXISTENZ Platform v0.76g (Private Key Signer)
+# EXISTENZ Platform v0.76i (Private Key Signer)
 # Copyright (c) 2026 by Gunther Voet. All Rights Reserved.
 # Released under strict Non-Commercial Open-Source License terms.
 # ==========================================================================
 # The glue, immutability,  protection part for a zero trust world.
 
-existentialCoreVersion                       = "v0.76g"
+existentialCoreVersion                       = "v0.76i"
 existentialCoreCheckMagic                    = b"EX25IMMUT32CORE7617"
-existentialCoreCheckSignature                = "7c165b322566e304975917cdc92de03d3ab14e72d1edaefc2ea18f7444ac891c"
+existentialCoreCheckSignature                = "b45624fc09463f05d2e9359db32ae928e084aa81e47bbd33ecde0a58d66d0656"
 
 class existentialCoreSignatures:
     existentialCore                              = "22023c142c21687803a3cdedb82684973d7ab5bb601b2b35d0bd8b448e26f99e"
-    existentialCoreThreatRoot                    = "57c413f8531731df0d2f09a260ea36c7e49269348b553fdeeaa2dd11e7bc4bb9"
-    existentialCoreThreatLegal                   = "6d07d97272d414f966ea7a9d7b2956b96541fecbcb9079f375408a62b3b6bd6e"
-    existentialCoreThreat                        = "759533dfd2a276046bc62985b17df7cefb999a1c3a07b7b983e5ee278d80302d"
+    existentialCoreThreatRoot                    = "1f95497bb174e069c2b727d8b72a7d556a03c0db451dcf2bac6b00bf191291ca"
+    existentialCoreThreatLegal                   = "931547edaba6ec457f2b6a22ef1961d56c08a765983036cb95642aa75fbd0ab1"
+    existentialCoreThreat                        = "90f9d7438a0497f54dbe065a64f5f5c111dee8c1184832c280d96c0bd2226689"
     existentialCoreCheck                         = "7c165b322566e304975917cdc92de03d3ab14e72d1edaefc2ea18f7444ac891c"
 
     existentialPublicKeys = (
