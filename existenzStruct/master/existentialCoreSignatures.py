6aa3d909eb9a860c63f27b31a0f365d2c7b3021b
Freaking Wildchild <32355099+gowildchild@users.noreply.github.com>
2026-08-21 02:15:24 +0200

    Update existentialCoreSignatures.py

--- existenzStruct/master/existentialCoreSignatures.py
+++ existenzStruct/master/existentialCoreSignatures.py
@@ -29,5 +29,4 @@ class existentialCoreSignatures:
         ("CoreThreatLegal", "cf73b021", "existentialCoreThreatLegalHash", "cf73b021381f426ad53910a83a1a5ce15ab6c3ea200e6e6818a805ca22328b13c7aacfc8a9cb5083abd633687e7693cc2c38310d026a36e59dfbd75cf592eb0b", 31, 2),
         ("CoreThreat", "19aaad78", "existentialCoreThreatHash", "19aaad78077aa3c5f0dc4f8eebf98e7c6b0df624d790a92b5d736c69771f4eb9586c55559c7b4cbf069a77ffefc678a12b4696958ac55a3ad9b9e6c05e040b02", 127, 3),
         ("CoreThreatShadowVacuum", "4f42af6a", "existentialCoreThreatShadowVacuumHash", "4f42af6a6be5078c95f186936058f8872553d523879b287e575f77e4180e4fc9a215e6bb30a6fd744ed85880945c133509ecf318ce4ca79caad6b7e1a50c1d01", 31, 5),
-        ("CoreChain", "11c39300", "existentialCoreChainHash", "11c393009d3f0dca9fc83c0c76c02a89737cfffbbcbb974cf2632d8d797e4dd42616ec3b8b625718314bbc146792259884300beae923af01cc26ab6713472308", 255, 9)
-    )
+        ("CoreChain", "11c39300", "existentialCor
