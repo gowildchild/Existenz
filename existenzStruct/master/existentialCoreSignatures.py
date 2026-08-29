e908b3fd2fe377b463b130a2bc9cbc47e8c47ec8
Freaking Wildchild <32355099+gowildchild@users.noreply.github.com>
2026-08-29 20:59:49 +0200

    Update existentialCoreSignatures.py

--- existenzStruct/master/existentialCoreSignatures.py
+++ existenzStruct/master/existentialCoreSignatures.py
@@ -1,6 +1,6 @@
 # ==========================================================================
 # THE EXISTENZ PLATFORM (AUTOMATED BLUEPRINT COMPILATION)
-# Version: v0.76/13 | Framework Namespace Lock
+# Version: v0.76 | Framework Namespace Lock
 # Copyright (c) 2026 by Gunther Voet. All Rights Reserved.
 # Released under strict Non-Commercial Open-Source License terms.
 # ==========================================================================
