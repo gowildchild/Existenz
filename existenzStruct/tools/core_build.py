45509baf042552c4e7183ba4e69c0a1cdf17adfc
Freaking Wildchild <32355099+gowildchild@users.noreply.github.com>
2026-08-30 02:43:20 +0200

    Update core_build.py

--- existenzStruct/tools/core_build.py
+++ existenzStruct/tools/core_build.py
@@ -892,7 +892,7 @@ def main():
                     if bitmask & 16:  tags.append("-DEV")
                     if bitmask & 32:  tags.append("-PSN")
                     temp_tags = " ".join(tags)
-                    return f"{hex(bitmask)}", str(seq), f"\033[91m[ ! NOT SIGNED ! ]\033[0m {temp_tags}", "├──", "│  ", "─ ──►" if is_last_in_group else "├──"
+                    return f"{hex(bitmask)}", str(seq), f"{temp_tags} \033[91m[ ! NOT SIGNED ! ]\033[0m", "├──", "│  ", " ► " if is_last_in_group else "├──"
                     
             return f"{hex(bitmask)}", str(seq), " ".join(tags), connector, v_line, chain_arrow
 
