7c079e19ebf0cf7ae5b2b24ec1e7ba91ddbaad30
Freaking Wildchild <32355099+gowildchild@users.noreply.github.com>
2026-08-30 14:14:58 +0200

    Update existentialCore.py

--- existenzStruct/master/existentialCore.py
+++ existenzStruct/master/existentialCore.py
@@ -3,7 +3,7 @@
 # Copyright (c) 2026 by Gunther Voet. All Rights Reserved. 
 # Released under strict Non-Commercial Open-Source License terms.
 # Commercial use requires immediate written license and explicit payment.
-# ==========================================================================
+# ==========================================================================c
 # VERSION: v0.76.14
 ##
 import hmac
