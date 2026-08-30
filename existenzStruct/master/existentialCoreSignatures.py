96a436e2694c38e9baa8dc4a540c4dab78f8be95
Freaking Wildchild <32355099+gowildchild@users.noreply.github.com>
2026-08-30 19:25:39 +0200

    Update existentialCoreSignatures.py

--- existenzStruct/master/existentialCoreSignatures.py
+++ existenzStruct/master/existentialCoreSignatures.py
@@ -10,7 +10,7 @@ existentialCoreCheckSignature                = "b36d1e03858491d3b12ddd1f4f304345
 
 class existentialCoreSignatures:
     existentialCore                              = "22023c142c21687803a3cdedb82684973d7ab5bb601b2b35d0bd8b448e26f99e"
-    existentialCores                             = "3c5fbbceb1b2a76ca4ca0acde4b947430311c0a3d884faab3c703b13f1267198"
+    existentialCores                             = "bb11656cb916db4245e1ac23cf9b04f03fddcc492faf420e9d0ecf54e4384af8"
     existentialCoreThreatRoot                    = "1f95497bb174e069c2b727d8b72a7d556a03c0db451dcf2bac6b00bf191291ca"
     existentialCoreThreatLegal                   = "931547edaba6ec457f2b6a22ef1961d56c08a765983036cb95642aa75fbd0ab1"
     existentialCoreThreatShadowVacuum            = "9b1d1bcf4903c7c26a6b75dd2e0c341ddab3594c2514c99e5d8e6b4651bfcc69"
