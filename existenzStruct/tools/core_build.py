0ff776424ca3e270b14032178fc17efb59359b01
Freaking Wildchild <32355099+gowildchild@users.noreply.github.com>
2026-08-30 07:32:24 +0200

    Update core_build.py

--- existenzStruct/tools/core_build.py
+++ existenzStruct/tools/core_build.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # ==========================================================================
 # THE EXISTENZ PLATFORM (Local Signing Suite & Cross-Compiler v0.76i)
-# Copyright (c) 2026 by Gunther Voet. All Rights Reserved. s
+# Copyright (c) 2026 by Gunther Voet. All Rights Reserved. 
 # ==========================================================================
 # FILE: existenzStruct/tools/core_build.py
 # 
