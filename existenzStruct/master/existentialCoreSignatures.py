d4d66b2f2c8d9f79cfbb1db12406e460732f6f05
Freaking Wildchild <32355099+gowildchild@users.noreply.github.com>
2026-08-30 07:43:37 +0200

    Update existentialCoreSignatures.py

--- existenzStruct/master/existentialCoreSignatures.py
+++ existenzStruct/master/existentialCoreSignatures.py
@@ -2,7 +2,7 @@
 # EXISTENZ CORE MASTER Private Key Signer v0.76.13
 # Copyright (c) 2026 by Gunther Voet. All Rights Reserved.
 # Released under strict Non-Commercial Open-Source License terms.
-# ==========================================================================
+# ==========================================================================s
 
 existentialCoreVersion                       = "v0.76.14"
 existentialCoreCheckMagic                    = b"EX25IMMUT32CORE7617"
