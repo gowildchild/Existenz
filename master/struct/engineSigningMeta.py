b14b94cf002ec01fc919dec550da5d36489f7dfa
Freaking Wildchild <32355099+gowildchild@users.noreply.github.com>
2026-09-02 15:27:54 +0200

    Update engineSigningMeta.py

--- master/struct/engineSigningMeta.py
+++ master/struct/engineSigningMeta.py
@@ -32,10 +32,36 @@ class existenzConfig:
         "SEPARATOR" = [",", ":"]
     }
 
+existenzLocations = {
+    "core": {
+        "Core":       "master/struct/existentialCore.py",
+        "Threat":     "master/struct/existentialCoreThreat.py",
+        "Signatures": "master/struct/existentialCoreSignatures.py",
+        "Check":      "master/existentialCoreCheck.py",
+        "Cores":      "master/existentialCores.json"
+    },
+    "manifest": {
+        "dist":   "dist",
+        "tools":  "dist/tools",
+        "build":  "master/build-tools",
+        "master": "master/struct"
+    },
+    "sign": {
+        "engineStruct":   "master/build-tools/engineStructure.py",
+        "engineLibrary":  "master/build-tools/engineLibrary.py",
+        "engineData":     "master/build-tools/engineData.py",
+        "structures":     "master/build-tools/engineStruct.py",
+        "jsonConfig":     "master/existentialSigningConfig.json",
+        "jsonSignatures": "master/existentialSignatures.json",
+        "jsonData":       "master/existentialSigningData.json",
+        "jsonManifest":   "manifest.json",
+        "jsonLocalConf":  "sign_integrity_config.json"
+    }
+}
+
 existenzPublicKeys = (
     ("Environment", "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPjuPmZWZS4tAjCxF1FkKtMfEroVnEThd+IIMXws9swd existenz-dev-gh@xsrv.net",  16,  7,  31,  existenzConfig.FINGERPRINT["Environment"]),
     ("Platform",    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ4tfhIlXUXCKvFE/HOwkVFTEIjWknHayefpjqTVAwSs existenz@xsrv.net",         32, 14,  46,  existenzConfig.FINGERPRINT["Platform"]),
     ("Developer",   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGHTQAOnKU4zaM03kASAKmrsps4ROCx8xMQZ4m12Yo8U existenz-dev-gwc@xsrv.net", 64,  8,  78,  existenzConfig.FINGERPRINT["Developer"]),
     ("Personal",    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKn1/r+k9+T5OJyoIjcrkj0DBmLq//x0/sffNMJNWofK existenz-dev-gv@xsrv.net", 128,  8, 142,  existenzConfig.FINGERPRINT["Personal"])
 )
-
