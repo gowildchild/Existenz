df5e188932bae4a64033b907bbdd76be7d0f5095
Freaking Wildchild <32355099+gowildchild@users.noreply.github.com>
2026-09-07 01:50:10 +0200

    Update cliStateInit.py

--- master/struct/module/cliStateInit.py
+++ master/struct/module/cliStateInit.py
@@ -1,9 +1,4 @@
 # ==========================================================================
-# EXISTENZ  master/struct/module/engineSigningInit.py
-# Copyright (c) 2026 by Gunther Voet. All Rights Reserved.
-# Released under strict Non-Commercial Open-Source License terms.
-# ==========================================================================
-# ==========================================================================
 # EXISTENZ  master/struct/module/cliStateInit.py
 # Copyright (c) 2026 by Gunther Voet. All Rights Reserved.
 # Released under strict Non-Commercial Open-Source License terms.
