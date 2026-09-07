faa6bc0baec2eeb1300e3e1448eab6b1f4553cce
Freaking Wildchild <32355099+gowildchild@users.noreply.github.com>
2026-09-07 11:20:48 +0200

    Update cliStateInit.py

--- master/struct/module/cliStateInit.py
+++ master/struct/module/cliStateInit.py
@@ -183,8 +183,9 @@ def execute(args, error_handler, repo_root: str):
                             else:
                                 struct_type = "PILLAR"
                                 
-                            if not bool(k == "NONE") and bool(pol & existenzCorePolicy.CORE_IMMUTABLE) and bool(pol & (existenzCorePolicy.CORE_PILLAR | existenzCorePolicy.CORE_RIGHTS | existenzCorePolicy.CORE_CANARY)):
+                            if not bool(k == "NONE") and bool(pol & existenzCorePolicy.CORE_IMMUTABLE) and bool(pol & (existenzCorePolicy.CORE_PILLAR | existenzCorePolicy.CORE_RIGHTS | existenzCorePolicy.CORE_CANARY | existenzCorePolicy.USER_CANARY)):
                                 calculated_basic.append(f'    "{k}"')
+
 
                             # Build entry strings with column formatting matching your target layout rules
                             line_entry = f'    "{k}":'.ljust(33)
