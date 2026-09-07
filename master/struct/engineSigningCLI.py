160cdf01d025f5236e8ad76395c7080006793513
Freaking Wildchild <32355099+gowildchild@users.noreply.github.com>
2026-09-07 16:13:54 +0200

    Update engineSigningCLI.py

--- master/struct/engineSigningCLI.py
+++ master/struct/engineSigningCLI.py
@@ -44,7 +44,7 @@ error_handler = visualmixErrorHandler(custom_post="_ERR")
 def main():
     parser = argparse.ArgumentParser(description="Existenz SHA256 Manifest")
     # Added "init" into the parser stage choices profile array
-    parser.add_argument("-stage", "--stage", choices=["test","init", "sign", "check", "verify", "manifest"], required=True, help="Manifest operation state selection.")
+    parser.add_argument("-stage", "--stage", choices=["test","init", "sign", "check", "verify", "manifest","integrity"], required=True, help="Manifest operation state selection.")
     parser.add_argument("-override", "--override", choices=["update", "recreate", "retry","newer"], required=True, help="Manifest operation override.")
     parser.add_argument("-run","--run", choices=["wet","dry"], default="wet", required=True, help="DRY shows only what it does, WET writes files")
     parser.add_argument("-circle", "--circle", choices=["dist","tools","build","master","all"], default="all", help="Select circle")
