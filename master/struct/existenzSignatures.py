55e409ed0b8a3456254d32a7ac1d0a25c808cba4
Freaking Wildchild <32355099+gowildchild@users.noreply.github.com>
2026-09-07 04:36:19 +0200

    Update existenzSignatures.py

--- master/struct/existenzSignatures.py
+++ master/struct/existenzSignatures.py
@@ -20,7 +20,6 @@ existentialToken = {
         "Check":              "b36d1e03858491d3b12ddd1f4f3043458be6065befb6f25622475b8bc909fd85",        
         "Schema":             "",
         "Cores":              "bb11656cb916db4245e1ac23cf9b04f03fddcc492faf420e9d0ecf54e4384af8",
-        "CoresChain":         "",
         "Threat":             "1f95497bb174e069c2b727d8b72a7d556a03c0db451dcf2bac6b00bf191291ca",
         "ThreatLegal":        "931547edaba6ec457f2b6a22ef1961d56c08a765983036cb95642aa75fbd0ab1",
         "ThreatShadowVacuum": "9b1d1bcf4903c7c26a6b75dd2e0c341ddab3594c2514c99e5d8e6b4651bfcc69",
