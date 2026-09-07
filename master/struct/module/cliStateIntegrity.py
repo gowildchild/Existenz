f47f101ee70e38fb7d06179569f6758b6824fd2c
Freaking Wildchild <32355099+gowildchild@users.noreply.github.com>
2026-09-07 18:32:56 +0200

    Update cliStateIntegrity.py

--- master/struct/module/cliStateIntegrity.py
+++ master/struct/module/cliStateIntegrity.py
@@ -9,8 +9,8 @@ import json
 import hashlib
 import hmac
 import engineSigningLibrary
-from engineSigningMeta import existenzLocations, existenzMeta, existenzIntegrityGlue, existenzSignatures
-from engineSigningStruct import existenzIntegrityKeysHandler
+from engineSigningMeta import existenzLocations, existenzMeta, existenzSignatures
+from engineSigningStruct import existenzIntegrityKeysHandler, existenzIntegrityGlue
 
 def execute(args, error_handler, repo_root: str):
     """
