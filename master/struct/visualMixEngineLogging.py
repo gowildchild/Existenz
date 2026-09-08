21b7c8f84641091e5bc147ef0d93a2f8fb865078
Freaking Wildchild <32355099+gowildchild@users.noreply.github.com>
2026-09-08 06:35:24 +0200

    Update visualMixEngineLogging.py

--- master/struct/visualMixEngineLogging.py
+++ master/struct/visualMixEngineLogging.py
@@ -1,4 +1,4 @@
-# ==========================================================================
+# ========================================================================== 
 # VisualMIX Engine : Logging
 # Copyright (c) 2026 by Gunther Voet. All Rights Reserved.
 # Released under strict Non-Commercial Open-Source License terms.
