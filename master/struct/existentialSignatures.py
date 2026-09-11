25abc0a883e51c3bb22a1e05addab61e78687f2f
Freaking Wildchild <32355099+gowildchild@users.noreply.github.com>
2026-09-11 12:06:28 +0200

    Update existentialSignatures.py

--- master/struct/existentialSignatures.py
+++ master/struct/existentialSignatures.py
@@ -1,5 +1,5 @@
 # ==========================================================================
-# EXISTENZ master/struct/existenzSignatures.py
+# EXISTENZ TEMPLATE master/struct/existenzSignatures.py
 # Copyright (c) 2026 by Gunther Voet. All Rights Reserved.
 # Released under strict Non-Commercial Open-Source License terms.
 # ==========================================================================
@@ -8,22 +8,22 @@ from engineSigningMeta import existenzLocations, existenzMeta
 
 existentialToken = {
     "MAGIC": {
-        "TAG":                existenzMeta.MAGIC["RAW"],
-        "TOKEN":              existenzMeta.MAGIC["TOKEN"],
-        "SIGNATURE":          existenzMeta.MAGIC["SIGNATURE"],
-        "REALM":              existenzMeta.HEADER["REALM"],
-        "VERSION":            existenzMeta.HEADER["VERSION"],
-        "AUTHOR":             existenzMeta.META["AUTHOR"]
+        "TAG":                "{{MAGIC_TAG}}",
+        "TOKEN":              "{{DYNAMIC_TOKEN}}",
+        "SIGNATURE":          "{{DYNAMIC_SIGNATURE}}",
+        "REALM":              "{{LIVE_REALM}}",
+        "VERSION":            "{{LIVE_VERSION}}",
+        "AUTHOR":             "{{LIVE_AUTHOR}}"
     },
     "master": {
-        "Core":               "22023c142c21687803a3cdedb82684973d7ab5bb601b2b35d0bd8b448e26f99e",
-        "Check":              "b36d1e03858491d3b12ddd1f4f3043458be6065befb6f25622475b8bc909fd85",        
-        "Schema":             "",
-        "Cores":              "bb11656cb916db4245e1ac23cf9b04f03fddcc492faf420e9d0ecf54e4384af8",
-        "Threat":             "1f95497bb174e069c2b727d8b72a7d556a03c0db451dcf2bac6b00bf191291ca",
-        "ThreatLegal":        "931547edaba6ec457f2b6a22ef1961d56c08a765983036cb95642aa75fbd0ab1",
-        "ThreatShadowVacuum": "9b1d1bcf4903c7c26a6b75dd2e0c341ddab3594c2514c99e5d8e6b4651bfcc69",
-        "ThreatSigned":       "23e9fbb89c801de638ddd73798b42f7c57af2bfde3e09a999f9527d9f27e39f3"
+        "Core":               "{{HASH_CORE}}",
+        "Check":              "{{HASH_CHECK}}",        
+        "Schema":             "{{HASH_SCHEMA}}",
+        "Cores":              "{{HASH_CORES}}",
+        "Threat":             "{{HASH_THREAT}}",
+        "ThreatLegal":        "{{HASH_THREAT_LEGAL}}",
+        "ThreatShadowVacuum": "{{HASH_THREAT_VACUUM}}",
+        "ThreatSigned":       "{{HASH_THREAT_SIGNED}}"
     },
     "chain": {
         "Core":               "",
@@ -37,25 +37,25 @@ existentialToken = {
         "master":             "master/struct"
     },
     "structs": {
-        "KeysPublic":         "40cc0fb5fe8bebcbd871f3089d701df607d727b689aa21ff2ec18350b91e1cc1",
-        "KeysHandler":        "b69be327173e6598c19958742a03eef59ed912574e92a106f2df6b1897d9fc48",
-        "KeysType":           "4336c5df5ba3017f8a706591fae29ec6ea38993cc653a165f6c8ae3181822557",
-        "Locations":          "ad0e774f35ff2000eaeb3f6063be74c3e86c043ed6a04bfd45b7ff2f6460bfa4"
+        "KeysPublic":         "{{HASH_KEYS_PUBLIC}}",
+        "KeysHandler":        "{{HASH_KEYS_HANDLER}}",
+        "KeysType":           "{{HASH_KEYS_TYPE}}",
+        "Locations":          "{{HASH_LOCATIONS}}"
     },
     "engine": {
-        "engineLogging":     "sha256 of master/struct/visualMixEngineLogging.py",
-        "engineCrypto":      "sha256 of master/struct/visualMixEngineCrypto.py",
-        "signingMeta":       "sha256 of master/struct/engineSigningMeta.py",
-        "signingStruct":     "sha256 of master/struct/engineSigningStruct.py",
-        "signingLibrary":    "sha256 of master/struct/engineSigningLibrary.py",
-        "builderLibrary":    "sha256 of master/struct/engineBuilderLibrary.py",
-        "cliStateTest" :     "sha256 of master/struct/module/cliStateTest.py",
-        "cliStateInit" :     "sha256 of master/struct/module/cliStateInit.py",
-        "cliStateManifest":  "sha256 of master/struct/module/cliStateManifest.py",
-        "cliStateSign":      "sha256 of master/struct/module/cliStateSign.py",
-        "cliStateVerify":    "sha256 of master/struct/module/cliStateVerify.py",
-        "cliStateBuild":     "sha256 of master/struct/module/cliStateBuild.py",
-        "Signatures":        "sha256 of master/existentialSignatures.json",
-        "Manifest":          "sha256 of manifest.json"
+        "engineLogging":      "{{HASH_LOGGING}}",
+        "engineCrypto":       "{{HASH_CRYPTO}}",
+        "signingMeta":        "{{HASH_SIGNING_META}}",
+        "signingStruct":      "{{HASH_SIGNING_STRUCT}}",
+        "signingLibrary":     "{{HASH_SIGNING_LIBRARY}}",
+        "builderLibrary":     "{{HASH_BUILDER_LIBRARY}}",
+        "cliStateTest" :      "{{HASH_CLI_TEST}}",
+        "cliStateInit" :      "{{HASH_CLI_INIT}}",
+        "cliStateManifest":   "{{HASH_CLI_MANIFEST}}",
+        "cliStateSign":       "{{HASH_CLI_SIGN}}",
+        "cliStateVerify":     "{{HASH_CLI_VERIFY}}",
+        "cliStateBuild":      "{{HASH_CLI_BUILD}}",
+        "Signatures":         "{{HASH_SIGNATURES_JSON}}",
+        "Manifest":           "{{HASH_MANIFEST_JSON}}"
     }
 }
