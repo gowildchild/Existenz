9f06abc057421c172e73cd03c5fe50d908601a31
Freaking Wildchild <32355099+gowildchild@users.noreply.github.com>
2026-09-12 06:03:24 +0200

    Update engineSigningStruct.py

--- master/struct/engineSigningStruct.py
+++ master/struct/engineSigningStruct.py
@@ -1,4 +1,4 @@
-# ==========================================================================
+# ========================================================================== 
 # EXISTENZ master/struct/engineSigningStruct.py 
 # Copyright (c) 2026 by Gunther Voet. All Rights Reserved.
 # Released under strict Non-Commercial Open-Source License terms.
