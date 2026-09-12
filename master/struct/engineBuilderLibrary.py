97f130dd8693eca3aa3d112bbeda5f1c5e09ff31
Freaking Wildchild <32355099+gowildchild@users.noreply.github.com>
2026-09-12 05:29:54 +0200

    Update engineBuilderLibrary.py

--- master/struct/engineBuilderLibrary.py
+++ master/struct/engineBuilderLibrary.py
@@ -1,4 +1,4 @@
-# ==========================================================================
+# ========================================================================== 
 # EXISTENZ  master/struct/engineBuilderLibrary.py  v0.7
 # Copyright (c) 2026 by Gunther Voet. All Rights Reserved.
 # Released under strict Non-Commercial Open-Source License terms.
